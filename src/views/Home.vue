549e244b6d3602c207887792e2f029114ad07801
DimasPondra <dimpullndra@gmail.com>
2021-03-17 14:41:26 +0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <BannerShayna />
+    <InstaShayna />
+    <LogoShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue'
+import HeroShayna from '@/components/HeroShayna.vue'
+import BannerShayna from '@/components/BannerShayna.vue'
+import InstaShayna from '@/components/InstaShayna.vue'
+import LogoShayna from '@/components/LogoShayna.vue'
+import FooterShayna from '@/components/FooterShayna.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    BannerShayna,
+    InstaShayna,
+    LogoShayna,
+    FooterShayna
+  }
+}
+</script>
